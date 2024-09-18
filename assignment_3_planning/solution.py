bba7bd3fe3c10717f9e77d96afc35e4513853e66
zebra314 <kidalfonso@gmail.com>
2024-09-18 19:05:16 +0200

    Add comments

--- assignment_3_planning/solution.py
+++ assignment_3_planning/solution.py
@@ -30,8 +30,19 @@ class AStar:
     self.closed_set = dict() # nodes already evaluated
 
   def evaluate_node(self):
+    """
+    Implement the A* algorithm to find the path from the start node to the target node
+
+    @input:
+      None, use the class variables set in the constructor
+
+    @return:
+      control_lst: list of steering angles
+      time_lst: list of simulation time
+    """
     self.plot_init()
 
+    # Init start node
     current_nd = self.Node(self.car.x0, self.car.y0, 0)
     current_nd.g = 0
     current_nd.h = self.get_dist(self.car.x0, self.car.y0, self.car.xt, self.car.yt)
@@ -39,6 +50,9 @@ class AStar:
     self.open_set[current_nd.idx] = current_nd
 
     while not len(self.open_set) == 0:
+
+      # Get the node with the lowest f value from the open set
+      # and move it to the closed set
       current_idx = min(self.open_set, key=lambda x: self.open_set[x].f)
       current_nd = self.open_set[current_idx]
       del self.open_set[current_idx]
@@ -57,14 +71,31 @@ class AStar:
         continue
       
       for neighbor in neighbors:
+
+        # Skip if the neighbor is already evaluated
         if neighbor.idx in self.closed_set:
           continue
+
+        # If the neighbor is already in the open set and has a lower f value
         elif neighbor.idx in self.open_set and neighbor.f < self.open_set[neighbor.idx].f:
           self.open_set[neighbor.idx] = neighbor
+
+        # If the neighbor is not in the open set or
+        # the cost to get to the neighbor from the current node is lower
         elif neighbor.idx not in self.open_set or neighbor.g < current_nd.g:
           self.open_set[neighbor.idx] = neighbor
 
   def get_track_path(self, node):
+    """
+    Trace back the path from the target node to the start node
+
+    @input:
+      node: current node
+
+    @return:
+      control_lst: list of steering angles
+      time_lst: list of simulation time
+    """
     control_lst, time_lst = [], []
     while node.parent != None:
       time_lst.insert(0, node.time * self.time_unit)
@@ -76,33 +107,11 @@ class AStar:
       node = node.parent
     time_lst.insert(0, 0)
     return control_lst, time_lst
-  
-  def plot_init(self):
-    if PLOT_ENABLED == True:
-      plt.ion()
-      self.fig, self.ax = plt.subplots()
-      self.ax.set_xlim([self.car.xlb, self.car.xub])
-      self.ax.set_ylim([self.car.ylb, self.car.yub])
-      self.ax.plot(self.car.xt, self.car.yt, 'bo')
-      for obs in self.car.obs:
-          obstacle = plt.Circle((obs[0], obs[1]), obs[2], color='blue', label='Obstacle')
-          plt.gca().add_artist(obstacle)
-  
-  def plot_close(self):
-    if PLOT_ENABLED == True:
-      plt.pause(1)
-      plt.close('all')
-  
-  def plot_point(self, x, y):
-    if PLOT_ENABLED == True:
-      self.ax.plot(x, y, 'ro')
-      plt.pause(0.001)
 
-  def get_dist(self, x1, y1, x2, y2):
-    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
-   
   def get_neighbors(self, node):
     """
+    Get the neighbors of the current node
+
     @input:
       node: current node
     
@@ -118,6 +127,7 @@ class AStar:
     for phi in phi_lst:
       nxt_node = self.get_nxt_node(node, phi) 
       
+      # If the position is reachable, initialize the new node
       if nxt_node is not None:
         nxt_node.phi = phi
         nxt_node.time = node.time + self.sim_times
@@ -143,11 +153,14 @@ class AStar:
     for _ in range(self.sim_times):
       x, y, theta = step(self.car, x, y, theta, phi, dt=self.time_unit)
   
+      # Check if the position is reachable
+      # If not, add the node to the closed set and return None
       if not self.check_reachable(x, y):
         node = self.Node(x, y, theta)
         self.closed_set[node.idx] = node
         return None
 
+      # Normalize theta
       while theta > np.pi:
         theta -= 2 * np.pi
       while theta < -np.pi:
@@ -157,6 +170,15 @@ class AStar:
     return nxt_node
 
   def check_reachable(self, x, y):
+    """
+    Check if the input position is reachable
+
+    @input:
+      x, y: position
+
+    @return:
+      Boolean: True if the position is reachable, False otherwise
+    """
     # Check collision
     for obs in self.car.obs:
       if self.get_dist(x, y, obs[0], obs[1]) <= obs[2] + 0.15:
@@ -166,6 +188,30 @@ class AStar:
     if self.car.xlb < x < self.car.xub and self.car.ylb < y < self.car.yub:
       return True
     return False
+  
+  def get_dist(self, x1, y1, x2, y2):
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+  
+  def plot_init(self):
+    if PLOT_ENABLED == True:
+      plt.ion()
+      self.fig, self.ax = plt.subplots()
+      self.ax.set_xlim([self.car.xlb, self.car.xub])
+      self.ax.set_ylim([self.car.ylb, self.car.yub])
+      self.ax.plot(self.car.xt, self.car.yt, 'bo')
+      for obs in self.car.obs:
+          obstacle = plt.Circle((obs[0], obs[1]), obs[2], color='blue', label='Obstacle')
+          plt.gca().add_artist(obstacle)
+  
+  def plot_close(self):
+    if PLOT_ENABLED == True:
+      plt.pause(1)
+      plt.close('all')
+  
+  def plot_point(self, x, y):
+    if PLOT_ENABLED == True:
+      self.ax.plot(x, y, 'ro')
+      plt.pause(0.001)
 
   class Node:
     def __init__(self, x, y, theta):
